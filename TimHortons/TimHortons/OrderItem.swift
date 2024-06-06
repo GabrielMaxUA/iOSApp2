5f9b5a5138bbfd54abde83c88ba65d1c6d2398b6
Max Gabriel <Max.Gabriel@triosstudent.com>
2024-06-05 23:59:13 -0400

    completed the code and adjusted delegates

---
+++ TimHortons/TimHortons/OrderItem.swift
@@ -0,0 +1,13 @@
+//
+//  OrderItem.swift
+//  TimHortons
+//
+//  Created by Max Gabriel on 2024-06-05.
+//
+
+import Foundation
+
+class OrderItem: NSObject {
+  var text = ""
+  var checked = false
+}
