b2f2295e7f8cddbdae44c3230e86e88f0a870206
Max Gabriel <Max.Gabriel@triosstudent.com>
2024-06-05 21:30:57 -0400

    adding story board and table controllers for the app in a story board

---
+++ TimHortons/TimHortons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimHortons
+//
+//  Created by Max Gabriel on 2024-06-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
